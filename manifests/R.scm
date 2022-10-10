a5263a653bae7e5194b7f366c18434f30ac32119
Kevin Kaiser <k8x1d@proton.me>
2022-10-10 16:05:40 -0400

    Add R support

---
+++ manifests/R.scm
@@ -0,0 +1,21 @@
+(specifications->manifest
+ '(
+   "r"
+
+   "libxml2" ;; xml c library
+   "openjdk" ;; java
+
+   ;; Libraries
+   "r-here"
+   "r-languageserver"
+   "r-tibble"
+   "r-future"
+   "r-readr"
+   "r-magrittr"
+   "r-plyr"
+   "r-dplyr"
+   "r-purrr"
+   "r-furrr"
+   "r-rjson"
+   "r-profvis"
+   ))
---
+++ manifests/music.scm
@@ -0,0 +1,5 @@
+(specifications->manifest
+ '(
+   "mpd-mpc" ;; cli interation with mpd
+   "ncmpcpp" ;; tui interation with mpd + tag editor
+   ))
