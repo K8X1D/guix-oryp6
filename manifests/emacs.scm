7a796b3da47f209394bd239f63e0e52ea29501c5
K8X1D <k8x1d@proton.me>
2022-12-11 08:36:01 -0500

    clean-up

--- manifests/emacs.scm
+++ manifests/emacs.scm
@@ -1,12 +1,16 @@
 (specifications->manifest
  '(
    ;; emacs distro
-   ;;"emacs"
+   "emacs"
    ;;"emacs-next"
-   "emacs-next-pgtk"
+   ;;"emacs-next-pgtk"
 
-   ;; straight
+   ;; Straight
    "git"
+
+   ;; doom
+   "ripgrep"
+   "fd"
 
    ;; vterm
    "cmake"
@@ -18,115 +22,29 @@
    ;; eglot ltex
    "openjdk" ;; java
 
-;;   ;; dashboard
-;;   ;;"emacs-dashboard"
-;;   "emacs-page-break-lines"
-;;
-;;   ;; mu4e
-;;   "pkg-config"
-;;   "meson"
-;;   "ninja"
-;;   "glib"
-;;   "gmime"
-;;   "xapian"
-;;
-;;   ;; Whichkey
-;;   "emacs-which-key"
-;;
-;;    ;;; Email client configuration
-;;   "isync"
-;;   "mu"
-;;   "msmtp"
-;;   "oauth2ms"
-;;   "emacs-mu4e-alert"
-;;
-;;
-;;   ;;;; packages
-;;   "emacs-straight-el"
-;;   "emacs-use-package"
-;;
-;;   ;; evil
-;;   "emacs-evil"
-;;   "emacs-evil-collection"
-;;   "emacs-evil-surround"
-;;   "emacs-undo-tree"
-;;   "emacs-evil-org"
-;;
-;;   ;; modeline
-;;   "emacs-minions"
-;;
-;;   ;; utils
-;;   ;;"emacs-vterm"
-;;   "emacs-markdown-mode"
-;;   "emacs-markdown-preview-mode"
-;;   "emacs-pdf-tools"
-;;   "emacs-pass"
-;;   "emacs-pinentry"
-   "pinentry-emacs"
-;;   "emacs-libmpdel"
-;;   "emacs-mpdel"
-;;
-;;   ;; completion
-;;   "emacs-vertico"
-;;   "emacs-orderless"
-;;   "emacs-marginalia"
-;;   "emacs-consult"
-;;   "emacs-embark"
-;;   "emacs-company"
-;;   "emacs-company-box"
-;;   ;;"emacs-eglot"
-;;
-;;   ;; themes
-;;   "emacs-doom-themes"
-;;   ;;"emacs-doom-modeline"
-;;   "emacs-mixed-pitch"
-;;
-;;   ;; org
-;;   "emacs-org"
-;;   "emacs-org-superstar"
-;;   "emacs-org-pomodoro"
-;;   "emacs-toc-org"
-;;   "emacs-org-roam"
-;;
-;;   ;; UI
-;;   "emacs-hl-todo"
-;;   "emacs-rainbow-mode"
-;;
-;;   ;; R
-;;   "emacs-ess"
-;;
-;;   ;; Python
-;;   "emacs-pyvenv"
+   ;; Taskjuggler
+   "ruby-taskjuggler"
+   
+   "zip" ;; allow export to odt
+   ;;
+   ;;
+   ;;   ;; Python
    "python-lsp-server"
-;;
-;;   ;; julia
-;;   "emacs-julia-mode"
-;;   ;;"emacs-julia-repl"
-;;
-;;   ;; git
-;;   ;;"emacs-magit"
-;;   ;;"emacs-magit-todos"
-   "emacs-forge"
-;;   ;;"emacs-sqlite"
-;;
-;;   ;; Markdown
-;;   "emacs-polymode"
-;;   ;;"emacs-polymode-markdown"
-;;   ;;"emacs-polymode-org"
-;;   "emacs-poly-r"
-;;
-;;   ;; guile scheme
-;;   "emacs-geiser-guile"
-;;   "emacs-paredit"
-;;
-;;   ;; Notebook
-;;   ;;"emacs-jupyter" ;; bug, see https://github.com/nnicandro/emacs-jupyter/issues/297
-;;   ;;"jupyter" ;; jupyter
-;;   ;;"python-ipykernel" ;; jupyter kernel for python
-;;
-;;
+   ;;
+   ;;
    ;; Fonts
    "font-fira-code"
    "font-fira-sans"
+
+
+   ;; file explorer
+   ;; https://github.com/alexluigit/dirvish
+   "fd"
+   "imagemagick"
+   "poppler"
+   "ffmpegthumbnailer"
+   "mediainfo"
+   "tar"
+   "unzip"
 
    ))
