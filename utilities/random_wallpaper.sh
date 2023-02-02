0a0023bd01a11459182e0aafc109450b261c34ec
K8X1D <k8x1d@protonmail.ch>
2023-02-02 10:54:03 -0500

    add wayland support

--- utilities/random_wallpaper.sh
+++ utilities/random_wallpaper.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-# from https://sylvaindurand.org/dynamic-wallpapers-with-sway/
-feh --bg-fill $(find ~/Pictures/wallpapers/paintings/*/*.{png,jpg} -type f | shuf -n1) &
-OLD_PID=$!
-while true; do
-    sleep 600
-    feh --bg-fill $(find ~/Pictures/wallpapers/paintings/*/*.{png,jpg} -type f | shuf -n1) &
-    NEXT_PID=$!
-    sleep 5
-    kill $OLD_PID
-    OLD_PID=$NEXT_PID
-done
+# adapted from from https://sylvaindurand.org/dynamic-wallpapers-with-sway/
+
+if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+		swaybg -i $(find ~/Pictures/wallpapers/paintings/*/*.{png,jpg} -type f | shuf -n1) -m fill &
+		OLD_PID=$!
+		while true; do
+				sleep 600
+				swaybg -i $(find ~/Pictures/wallpapers/paintings/*/*.{png,jpg} -type f | shuf -n1) -m fill &
+				NEXT_PID=$!
+				sleep 5
+				kill $OLD_PID
+				OLD_PID=$NEXT_PID
+		done
+else
+		feh --bg-fill $(find ~/Pictures/wallpapers/paintings/*/*.{png,jpg} -type f | shuf -n1) &
+		OLD_PID=$!
+		while true; do
+				sleep 600
+				feh --bg-fill $(find ~/Pictures/wallpapers/paintings/*/*.{png,jpg} -type f | shuf -n1) &
+				NEXT_PID=$!
+				sleep 5
+				kill $OLD_PID
+				OLD_PID=$NEXT_PID
+		done
+fi
+
+
