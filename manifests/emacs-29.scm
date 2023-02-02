8ea25dd04db0aa081113f94dada4697aaddbc4b5
K8X1D <k8x1d@protonmail.ch>
2023-02-02 10:54:56 -0500

    manifest for emacs 29

--- manifests/emacs-29.scm
+++ manifests/emacs-29.scm
@@ -1,14 +1,13 @@
+;; see https://guix.gnu.org/manual/en/html_node/Defining-Package-Variants.html
 (use-modules (gnu packages emacs)
-						 (guix packages)
-						 (guix transformations))
+             (guix packages)
+             (guix transformations))
 
 (define transform
-	(options->transformation
-	 '((with-branch . "emacs-next=emacs-29"))))
+  (options->transformation
+   '((with-branch . "emacs-next=emacs-29"))))
 
-;;(packages->manifest (list (transform emacs-next))) ;; missing git...
-(concatenate-manifests
- (list (specifications->manifest
-				'("coreutils"
-					"git"))
-			 (packages->manifest (list (transform emacs-next)))))	 
+;;(packages->manifest (list (transform emacs-next)))
+
+(packages->manifest
+ (list (transform (specification->package "emacs-next"))))
